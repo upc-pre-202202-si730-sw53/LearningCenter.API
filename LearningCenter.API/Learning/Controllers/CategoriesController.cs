039e207e2e4b0eefac4ff39d557191d3b22807f6
Angel Velasquez <pcisavel@upc.edu.pe>
2022-10-12 12:44:55 -0500

    feat(categories): added initial action for categories controller.

---
+++ LearningCenter.API/Learning/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+using System.Net.Mime;
+using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Learning.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningCenter.API.Learning.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class CategoriesController : ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoriesController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Category>> GetAllAsync()
+    {
+        var categories = await _categoryService.ListAsync();
+        return categories;
+    }
+    
+    
+}
