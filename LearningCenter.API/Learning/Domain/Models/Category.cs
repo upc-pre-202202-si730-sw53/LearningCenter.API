ae51bb6c6e0997f4bed4741657a0bde9d020f25c
Angel Velasquez <pcisavel@upc.edu.pe>
2022-10-13 16:42:22 -0500

    feat(tutorials): added tutorial entity and its relationship with category.

--- LearningCenter.API/Learning/Domain/Models/Category.cs
+++ LearningCenter.API/Learning/Domain/Models/Category.cs
@@ -2,6 +2,14 @@ namespace LearningCenter.API.Learning.Domain.Models;
 
 public class Category
 {
+    public Category()
+    {
+        Tutorials = new List<Tutorial>();
+    }
+
     public int Id { get; set; }
     public string Name { get; set; }
+    
+    // Relationships
+    public List<Tutorial> Tutorials { get; set; }
 }
