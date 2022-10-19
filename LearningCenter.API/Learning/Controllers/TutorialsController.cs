84e3f19736ddea7a14a969ca35aa679f6930cdc0
Angel Velasquez <pcisavel@upc.edu.pe>
2022-10-19 12:44:05 -0500

    feat(tutorials): added controller.

---
+++ LearningCenter.API/Learning/Controllers/TutorialsController.cs
@@ -0,0 +1,61 @@
+using AutoMapper;
+using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Learning.Domain.Services;
+using LearningCenter.API.Learning.Resources;
+using LearningCenter.API.Shared.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningCenter.API.Learning.Controllers;
+
+[ApiController]
+[Route("")]
+public class TutorialsController : ControllerBase
+{
+    private readonly ITutorialService _tutorialService;
+    private readonly IMapper _mapper;
+
+    public TutorialsController(ITutorialService tutorialService, IMapper mapper)
+    {
+        _tutorialService = tutorialService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<TutorialResource>> GetAllAsync()
+    {
+        var tutorials = await _tutorialService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<Tutorial>, IEnumerable<TutorialResource>>(tutorials);
+        return resources;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] SaveTutorialResource resource)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState.GetErrorMessages());
+
+        var tutorial = _mapper.Map<SaveTutorialResource, Tutorial>(resource);
+
+        var result = await _tutorialService.SaveAsync(tutorial);
+
+        if (!result.Success)
+            return BadRequest(result.Message);
+
+        var tutorialResource = _mapper.Map<Tutorial, TutorialResource>(result.Resource);
+
+        return Ok(tutorialResource);
+    }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+}
