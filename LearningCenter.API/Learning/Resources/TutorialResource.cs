dcda24b465f8d2bfbe13f98d9d97aab66ce69de6
Angel Velasquez <pcisavel@upc.edu.pe>
2022-10-19 12:30:08 -0500

    feat(tutorials): implemented object mapping configuration.

---
+++ LearningCenter.API/Learning/Resources/TutorialResource.cs
@@ -0,0 +1,9 @@
+namespace LearningCenter.API.Learning.Resources;
+
+public class TutorialResource
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public CategoryResource Category { get; set; }
+}
