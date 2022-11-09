d710ef1f0fa9387f2f15e06d247c78ec0c9bd1e2
Angel Velasquez <pcisavel@upc.edu.pe>
2022-11-09 11:41:15 -0500

    feat(security): added jwt middleware.

---
+++ LearningCenter.API/Security/Authorization/Middleware/JwtMiddleware.cs
@@ -0,0 +1,38 @@
+using LearningCenter.API.Security.Authorization.Handlers.Interfaces;
+using LearningCenter.API.Security.Authorization.Settings;
+using LearningCenter.API.Security.Domain.Services;
+
+namespace LearningCenter.API.Security.Authorization.Middleware;
+
+public class JwtMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly AppSettings _appSettings;
+
+    public JwtMiddleware(RequestDelegate next, AppSettings appSettings)
+    {
+        _next = next;
+        _appSettings = appSettings;
+    }
+
+    public async Task Invoke(
+        HttpContext context,
+        IUserService userService,
+        IJwtHandler handler)
+    {
+       // Get Token
+       var token = context.Request.Headers["Authorization"]
+           .FirstOrDefault()?
+           .Split(" ")
+           .Last();
+       // Extract userId
+       var userId = handler.ValidateToken(token);
+
+       // Store in context if not null
+       if (userId != null)
+           context.Items["User"] = await userService.GetByIdAsync(userId.Value);
+       
+       // Call next in chain
+       await _next(context);
+    }
+}
