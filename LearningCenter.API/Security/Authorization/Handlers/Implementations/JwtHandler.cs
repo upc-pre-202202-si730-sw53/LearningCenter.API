28ba4815827dcb9d1d58f16a57d6d40a859de1db
Angel Velasquez <pcisavel@upc.edu.pe>
2022-11-09 11:10:11 -0500

    feat(security): implemented token handler.

---
+++ LearningCenter.API/Security/Authorization/Handlers/Implementations/JwtHandler.cs
@@ -0,0 +1,81 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using LearningCenter.API.Security.Authorization.Handlers.Interfaces;
+using LearningCenter.API.Security.Authorization.Settings;
+using LearningCenter.API.Security.Domain.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LearningCenter.API.Security.Authorization.Handlers.Implementations;
+
+public class JwtHandler : IJwtHandler
+{
+    private readonly AppSettings _appSettings;
+
+    public JwtHandler(IOptions<AppSettings> appSettings)
+    {
+        _appSettings = appSettings.Value;
+    }
+
+    public string GenerateToken(User user)
+    {
+        // Generate Token for a valid period of 7 days
+        
+        Console.WriteLine($"Secret: {_appSettings.Secret}");
+        var secret = _appSettings.Secret;
+        var key = Encoding.ASCII.GetBytes(secret);
+        Console.WriteLine($"Secret key: {key.Length}");
+        Console.WriteLine($"User Id: {user.Id.ToString()}");
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Sid, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+            }),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha512Signature)
+        };
+        var tokenHandler = new JwtSecurityTokenHandler();
+        
+        Console.WriteLine($"Token Expiration: {tokenDescriptor.Expires.ToString()}");
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+
+    public int? ValidateToken(string token)
+    {
+        // If token is null or empty
+        if (string.IsNullOrEmpty(token))
+            // Return null 
+            return null;
+        // Otherwise, perform validation
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+        try
+        {
+            tokenHandler.ValidateToken(token, new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                // Expiration without delay
+                ClockSkew = TimeSpan.Zero
+            }, out var validatedToken);
+            var jwtToken = (JwtSecurityToken)validatedToken;
+            var userId = int.Parse(jwtToken.Claims.First(claim => claim.Type == ClaimTypes.Sid).Value);
+            return userId;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return null;
+        }
+    }
+}
